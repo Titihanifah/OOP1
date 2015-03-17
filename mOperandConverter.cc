91d4c6aad17c9ef84eab490a9bf36a5520038de5
elvanowen <vanzz_95@hotmail.com>
2015-03-17 18:00:10 +0700

    operand converter fully functional

--- mOperandConverter.cc
+++ mOperandConverter.cc
@@ -7,8 +7,8 @@ using namespace std;
 
 int main()
 {
-	//string in("X + II / V");
-	string in("true & true ^ false | false");
+	//string in("X / IX * VI + II / V");
+	string in("!true & !true ^ !false | false");
 	OperandConverter op;
 	op.setExpression(in);
 	string out = op.toArabicExpression();
