c26382eda01f51536c28a08fc97c7083592ea804
elvanowen <vanzz_95@hotmail.com>
2015-03-19 09:49:21 +0700

    added date

--- CommandHistory.cc
+++ CommandHistory.cc
@@ -116,7 +116,7 @@ void CommandHistory::save()
 	
 	time_t t = time(0);   // get time now
 	struct tm * now = localtime( & t );
-	fprintf(fp,"%d - %d - %d\n\n",now->tm_year + 1900),(now->tm_mon + 1),now->tm_mday);
+	fprintf(fp,"%d - %d - %d , %d:%d:%d\n\n",(now->tm_year + 1900),(now->tm_mon + 1),now->tm_mday,now->tm_hour,now->tm_min,now->tm_sec);
 	
 	// popping every last histories for saving to file from save Stack
 	while(!saveStack.isEmpty())
